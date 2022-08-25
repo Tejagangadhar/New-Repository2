1a0707ed03b3ab44bf4b5c28dec48de94beba2ab
Tejagangadhar <tejagangadhar9133@gmail.com>
2022-08-25 17:25:31 +0530

    secondary name

---
+++ src/AmountCredited/Amount.java
@@ -0,0 +1,16 @@
+package AmountCredited;
+
+public class Amount {
+	public static void main(String[] args) {
+		int moeny=800;
+		if (moeny>=500) {
+			System.out.println("Eligible to Withdraw");
+		}
+		else {
+			System.out.println("Not Eligible to Withdraw");
+			
+		}
+	}
+	
+
+}
